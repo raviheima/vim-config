0456ca0b17ec2718e84930d86ebe9bb2eac80e84
raviheima <iicanbeyourherobaby@gmail.com>
2025-12-31 10:56:54 +0100

    update discord presence

--- lua/plugins/discord.lua
+++ lua/plugins/discord.lua
@@ -1,10 +1,17 @@
-return {{
-  'vyfor/cord.nvim',
-  build = ':Cord update',
-   opts = {
-        text = {
-
-            workspace = "Avoid spaghetti code.",
-        },
-   },
-}}
+return {
+  {
+    'vyfor/cord.nvim',
+    build = ':Cord update',
+    opts = {
+      text = {
+        workspace = "Avoid spaghetti code.",
+        editing = function(opts)
+          return string.format('Editing %s:[%d/%d]', opts.filename, opts.cursor_line, vim.api.nvim_buf_line_count(0))
+        end,
+        viewing = function(opts)
+          return string.format('Viewing %s:[%d/%d]', opts.filename, opts.cursor_line, vim.api.nvim_buf_line_count(0))
+        end,
+      },
+    },
+  },
+}
