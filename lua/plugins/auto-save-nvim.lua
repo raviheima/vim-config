badbc4f14989de32a1e28643e7b1648d659b2dc2
raviheima <iicanbeyourherobaby@gmail.com>
2026-01-07 14:43:52 +0100

    add a better auto save plugin

--- lua/plugins/auto-save-nvim.lua
+++ lua/plugins/auto-save-nvim.lua
@@ -1,9 +1,10 @@
-return {
-  {
-    "pocco81/auto-save.nvim",
-    config = function()
-      require("auto-save").setup({})
-    end,
+return { {
+  "okuuva/auto-save.nvim",
+  version = '^1.0.0', -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+  cmd = "ASToggle", -- optional for lazy loading on command
+  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+  opts = {
+    -- your config goes here
+    -- or just leave it empty :)
   },
-}
-
+  },}
